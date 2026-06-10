fcf03b502467c46dd02c63b19fef942e62549fd6
Danindra <danindramaulalhaq@gmail.com>
2026-06-10 12:00:44 +0700

    update add command

---
+++ app/Console/Commands/GenerateUser.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
+use Illuminate\Console\Command;
+use Hash;
+
+use App\Models\User;
+
+#[Signature('app:generate-user {email} {password} {role}')]
+#[Description('Command description')]
+class GenerateUser extends Command
+{
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $email = $this->argument('email');
+        $password = $this->argument('password');
+        $role = $this->argument('role');
+
+        if(User::where('email', $email)->exists()) {
+            $this->error('User already exists.');
+            return;
+        }
+
+        $user = User::create([
+            'name' => $email,
+            'email' => $email,
+            'password' => Hash::make($password),
+            'role' => $role,
+        ]);
+
+        $this->info('User created successfully.');
+    }
+}
